903bf36abbeb7f9addd9d442edb1844af02032a8
Varun <varunkverma1996@gmail.com>
2023-10-31 08:12:36 +0530

    registered a fileserver in routes to serve static content like images

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -19,5 +19,9 @@ func routes(app *config.AppConfig) http.Handler {
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
 
+	// creating a file server
+	fs := http.FileServer(http.Dir("../../static/"))
+	mux.Handle("/static/*", http.StripPrefix("/static", fs))
+
 	return mux
 }
