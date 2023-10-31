038ab395c2e397f860d8a086d3756e50b83f65de
Varun <varunkverma1996@gmail.com>
2023-10-31 07:44:28 +0530

    initial setup

---
+++ cmd/web/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/alexedwards/scs/v2"
+	"github.com/varunkverma/bookings/pkg/config"
+	"github.com/varunkverma/bookings/pkg/handlers"
+	"github.com/varunkverma/bookings/pkg/render"
+)
+
+const portNumber = ":3000"
+
+var app config.AppConfig
+var session *scs.SessionManager
+
+// main is the main function
+func main() {
+	// change this to true when in production
+	app.InProduction = false
+
+	// set up the session
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	app.Session = session
+
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("cannot create template cache")
+	} else {
+		fmt.Println("Template Cache created in the beginning of program")
+	}
+
+	app.TemplateCache = tc
+	app.UseCache = false
+
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandlers(repo)
+
+	render.NewTemplates(&app)
+
+	fmt.Printf("Staring application on port %s\n", portNumber)
+
+	srv := &http.Server{
+		Addr:    portNumber,
+		Handler: routes(&app),
+	}
+
+	err = srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
